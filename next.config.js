bc2f1ca9b55582d07c813c695730c37a2a993637
Abdullah Qureshi <adnx1080@gmail.com>
2022-06-26 22:54:13 +0500

    project-setup

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    urlImports: [
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/",
+    ],
+  },
+};
+
+module.exports = nextConfig;
